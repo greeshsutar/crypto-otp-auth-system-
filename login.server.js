93033f9ecab708ec3b8352c79700348037c466cd
Greesh Sutar <girishasutar223@gmail.com>
2026-04-17 16:34:01 +0530

    added path

--- login.server.js
+++ login.server.js
@@ -1,36 +1,35 @@
 let express = require("express");
-let MongoConnect =require("./configure/login.configuare");
+let MongoConnect = require("./configure/login.configuare");
 const cors = require("cors");
 let Routing = require("./router/login.route");
 require("dotenv").config();
-// instance entire application 
-let app = express(); //create
+
+const path = require("path");
+
+let app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
-let authMiddleware = require("./auth/auth");
-const { getProfile } = require("./controller/login.controller");
 
+// serve frontend
+app.use(express.static(path.join(__dirname, "view")));
 
-// middleware body parsing 
-app.get("/user/profile", authMiddleware, getProfile);
-
-
-//cors
-
-
-
-//route
+// routes
 Routing(app);
 
-// call function to connect Db
+// root route
+app.get("/", (req, res) => {
+   res.send("Hello How are You");
+});
+
+// error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ message: "Something went wrong" });
 });
 
-app.get("/",(req,res)=>{
-   res.send("Hello How are You")
-})
+// start server
 const PORT = process.env.PORT || 3080;
 MongoConnect().then(() => {
   app.listen(PORT, () => {
